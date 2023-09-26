ef36192ac79a6edb50b3ed35298f555a62c89650
MGConil <germanconil@hotmail.com>
2023-09-26 11:10:16 -0300

    cambio de estilos

--- components/Calculation.tsx
+++ components/Calculation.tsx
@@ -1,3 +1,5 @@
+
+
 import React from 'react'
 import { Image, Text, TouchableOpacity, View } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
@@ -6,6 +8,7 @@ import { StyleSheet } from "react-native"
 import { removeAllUsers } from '../redux/action'
 import { SafeAreaView } from 'react-native'
 import { ScrollView } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
 
 
 export interface Acount {
@@ -24,37 +27,48 @@ function Calculation({ navigation }: any) {
 
     const handleCalculateAccounts = () => {
         dispatch(removeAllUsers())
-        navigation.navigate('Home')
+        setTimeout(() => {
+            
+            navigation.navigate('Home')
+        }, 300);
     }
 
     return (
 
         <SafeAreaView style={styles.containerPage}>
-            <ScrollView>
-                <View>
-                    <View style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-                        {acount.map((item,index) =>
-                            <View style={styles.containerResult} key={index}>
-                                <View style={styles.listAcount} >
-                                    <Text style={{ fontSize: 22, fontWeight: "800" }}>🙂 {item[0]}</Text>
-                                    <Text >    paga a..   </Text>
-                                    <Text style={{ fontSize: 22, fontWeight: "800" }}>🙂 {item[1]}</Text>
+            <LinearGradient
+                colors={['#4A1955', '#1F1146']}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1.5, y: 0.5 }}
+                style={styles.linearGradient}
+            >
+                <ScrollView>
+                    <View>
+                        <View style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+                            {acount.map((item, index) =>
+                                <View style={styles.containerResult} key={index}>
+                                    <View style={styles.listAcount} >
+                                        <Text style={{ fontSize: 22, fontWeight: "800" }}>🙂 {item[0]}</Text>
+                                        <Text >    paga a..   </Text>
+                                        <Text style={{ fontSize: 22, fontWeight: "800" }}>🙂 {item[1]}</Text>
+                                    </View>
+                                    <View style={styles.price}>
+                                        <Text style={{ fontSize: 28, fontWeight: "900", color: "#4A1955" }}>$ {(item[2].toFixed(2))}</Text>
+                                    </View>
                                 </View>
-                                <View style={styles.price}>
-                                    <Text style={{ fontSize: 28, fontWeight: "900", color: "#4A1955" }}>$ {(item[2].toFixed(2))}</Text>
-                                </View>
-                            </View>
-                        )}
-                        <TouchableOpacity
-                            style={styles.splitButton}
-                            onPress={handleCalculateAccounts}>
-                            <Text style={{ color: "black", fontSize: 17, fontWeight: "500" }}>Nueva cuenta</Text>
-                        </TouchableOpacity>
-                        <Image source={require('../assets/CuentaJustaLogo.png')} style={{ width: "80%", height: 100, marginTop: 30 }} />
+                            )}
+                            <TouchableOpacity
+                                style={styles.splitButton}
+                                onPress={handleCalculateAccounts}>
+                                <Text style={{ color: "black", fontSize: 17, fontWeight: "500" }}>Nueva cuenta</Text>
+                            </TouchableOpacity>
+                            <Image source={require('../assets/Logo_Sin_Fondo.png')} style={{ width: "80%", height: 100, marginTop: 30 }} />
 
+                        </View>
                     </View>
-                </View>
-            </ScrollView>
+                </ScrollView>
+            </LinearGradient>
+
         </SafeAreaView>
 
     )
@@ -74,13 +88,13 @@ const styles = StyleSheet.create({
         justifyContent: "center",
 
     },
-    price:{
-        backgroundColor:"#DFDCD3",
-        width:"50%",
-        borderRadius:10,
-        display:"flex",
-        alignItems:"center",
-        marginTop:3
+    price: {
+        backgroundColor: "#DFDCD3",
+        width: "50%",
+        borderRadius: 10,
+        display: "flex",
+        alignItems: "center",
+        marginTop: 3
     },
     splitButton: {
         alignItems: 'center',
@@ -105,24 +119,29 @@ const styles = StyleSheet.create({
         borderRadius: 40,
         opacity: 0.1,
     },
+    linearGradient: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        paddingTop:100
+    },
     containerPage: {
         flex: 1,
         backgroundColor: '#4A1955',
         alignItems: 'center',
         justifyContent: "flex-start",
-        paddingTop: 50,
         flexDirection: "column",
         width: "100%", // Ancho del 100%
         height: '100%'
     },
     containerResult: {
-        width: "100%",
+        width: "90%",
         textAlign: "center",
         alignItems: "center",
         justifyContent: "center",
         backgroundColor: "#F0EEE8",
         marginTop: 10,
-        padding: 10,
+        padding: 5,
         borderRadius: 30,
         elevation: 7,
         shadowColor: 'rgba(255,255, 255,1)',
--- components/Home.tsx
+++ components/Home.tsx
@@ -49,6 +49,7 @@ function Home({ navigation }: any) {
 
     }
 
+  
 
     // estado para controlar disabled del boton cargar persona 
     const [disabled, setdisabled] = useState(true)
@@ -75,54 +76,67 @@ function Home({ navigation }: any) {
 
     }
 
+
+
     return (
 
         <SafeAreaView style={styles.containerPage}>
-            <ScrollView>
-                <View style={styles.container}>
-                    <Text style={{ color: "white", fontWeight: "500", fontSize: 18 }}>Ingresar persona</Text>
-                    <TextInput
-                        placeholder='Persona..'
-                        style={styles.input}
-                        onChangeText={onChangeInputAmount}
-                        value={input.user}
-                    ></TextInput>
-                    <Text style={{ color: "white", fontWeight: "500", fontSize: 18 }}>Ingresar monto </Text>
-                    <TextInput
-                        placeholder='Monto..'
-                        style={styles.input}
-                        onChangeText={onChangeInputDescr}
-                        value={(input.amount)}
-                        keyboardType="numeric"
+            <LinearGradient
+                colors={['#4A1955', '#1F1146']}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1.5, y: 0.5 }}
+                style={styles.linearGradient}
+            >
 
-                    ></TextInput>
-                    <TouchableOpacity
-                        style={disabled ? styles.buttonAddDisabled : styles.buttonAdd}
-                        onPress={addUserToList}
-                        disabled={disabled}
-                    >
-                        <Text
-                            style={{ color: "black", fontSize: 17, fontWeight: "500" }}>CARGAR
-                        </Text>
-                    </TouchableOpacity>
-                    {!totalAmount.length ? (
-                        <Image source={require('../assets/CuentaJustaLogo.png')} style={{ width: "80%", height: 100, marginTop: 60 }} />
-                    ) : null}
-                </View>
-                <View style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-                    {totalAmount.length ? (
-                        <Text style={{ marginTop: 20, fontWeight: "800", color: "#FFC300", marginBottom: 20, fontSize: 19 }}>TOTAL GASTOS: $ {suma.toLocaleString()} </Text>
+                <ScrollView>
+                    <View style={styles.container}>
+                        <Text style={{ color: "#ECE5F1", fontWeight: "500", fontSize: 18, marginBottom: 10 }}>Ingresar persona</Text>
+                        <TextInput
+                            placeholder='Persona..'
+                            style={styles.input}
+                            onChangeText={onChangeInputAmount}
+                            value={input.user}
+                        ></TextInput>
+                        <Text style={{ color: "#ECE5F1", fontWeight: "500", fontSize: 18, marginTop: 15, marginBottom: 10 }}>Ingresar monto </Text>
+                        <TextInput
+                            placeholder='Monto..'
+                            style={styles.input}
+                            onChangeText={onChangeInputDescr}
+                            value={(input.amount)}
+                            keyboardType="numeric"
 
-                    ) : null}
-                    <Lista />
-                    <TouchableOpacity
-                        style={totalAmount.length > 1 ? styles.buttonAdd : styles.buttonAddDisabled}
-                        onPress={handleCalculateAccounts}
-                        disabled={totalAmount.length > 1 ? false : true} >
-                        <Text style={{ color: "black", fontSize: 17, fontWeight: "500" }}>CALCULAR</Text>
-                    </TouchableOpacity>
-                </View>
-            </ScrollView>
+                        ></TextInput>
+                        <TouchableOpacity
+                            style={disabled ? styles.buttonAddDisabled : styles.buttonAdd}
+                            onPress={addUserToList}
+                            disabled={disabled}
+                        >
+                            <Text
+                                style={{ color: "black", fontSize: 17, fontWeight: "500" }}>CARGAR
+                            </Text>
+                        </TouchableOpacity>
+                        {!totalAmount.length ? (
+                            <Image source={require('../assets/Logo_Sin_Fondo.png')} style={{ width: "80%", height: 100, marginTop: 20 }} />
+                        ) : null}
+                    </View>
+                    <View style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+                        {totalAmount.length ? (
+                            <Text style={{ marginTop: 20, fontWeight: "800", color: "#FFC300", marginBottom: 20, fontSize: 19 }}>TOTAL GASTOS: $ {suma.toLocaleString()} </Text>
+
+                        ) : null}
+                        <View style={styles.horizontalLine} />
+                        <Lista />
+                        <TouchableOpacity
+                            style={totalAmount.length > 1 ? styles.buttonAdd : styles.buttonAddDisabledCalc}
+                            onPress={handleCalculateAccounts}
+                            disabled={totalAmount.length > 1 ? false : true} >
+                            <Text style={{ color: "black", fontSize: 17, fontWeight: "500" }}>CALCULAR</Text>
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
+
+            </LinearGradient>
+
         </SafeAreaView>
 
     )
@@ -137,27 +151,35 @@ const styles = StyleSheet.create({
         height: 40,
         width: 300,
         margin: 10,
+        marginTop: 5,
         borderWidth: 0,
-        backgroundColor: "#ECEDF4",
+        backgroundColor: "#E1E0E8",
         padding: 9,
-        borderRadius: 300,
+        borderRadius: 8,
         fontSize: 14,
-        opacity: 0.7,
         fontWeight: "600",
-        elevation: 9,
+        elevation: 3,
         shadowColor: 'rgba(255, 255, 255,1)',
     },
     linearGradient: {
         flex: 1,
         width: '100%',
-        height: '100%'
+        height: '100%',
+        paddingTop:40
+
+    },
+    horizontalLine: {
+        borderBottomColor: '#DEDDE1',
+        width: "80%", // Color de la línea
+        borderBottomWidth: 1,      // Ancho de la línea
+        marginVertical: 20,
+        opacity: 0.7    // Espacio vertical alrededor de la línea
     },
     containerPage: {
         flex: 1,
         backgroundColor: '#4A1955',
         alignItems: 'center',
         justifyContent: "flex-start",
-        paddingTop: 50,
         flexDirection: "column",
         width: "100%", // Ancho del 100%
         height: '100%'
@@ -168,6 +190,7 @@ const styles = StyleSheet.create({
         alignItems: "center",
         alignContent: "center",
         alignSelf: "center",
+        marginTop: 40
     },
 
     buttonAdd: {
@@ -186,6 +209,17 @@ const styles = StyleSheet.create({
     buttonAddDisabled: {
         alignItems: 'center',
         justifyContent: "center",
+        backgroundColor: '#FFC300',
+        padding: 6,
+        margin: 20,
+        height: 40,
+        width: 150,
+        borderRadius: 40,
+        opacity: 0.3
+    },
+    buttonAddDisabledCalc: {
+        alignItems: 'center',
+        justifyContent: "center",
         backgroundColor: '#B3B3B3',
         padding: 6,
         margin: 20,
@@ -193,8 +227,9 @@ const styles = StyleSheet.create({
         width: 150,
         borderRadius: 40,
         opacity: 0
-    },
+    }
 
 
 });
+
 
